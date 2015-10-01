ad93ac238ef60a2dd5c9d91a4f117dd0db87677c
nate-rcl <nsarr@library.rochester.edu>
2015-10-01 10:06:16 -0400

    initial commit

---
+++ templates/islandora-newspaper.tpl.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for the object page for newspaper
+ *
+ * Available variables:
+ * - $islandora_content: A rendered vertical tabbed newspapper issue browser.
+ * - $parent_collections: An array containing parent collection IslandoraFedoraObject(s).
+ * - $description: Rendered metadata descripton for the object.
+ * - $metadata: Rendered metadata display for the binary object.
+ *
+ * @see template_preprocess_islandora_newspaper()
+ */
+?>
+<div class="islandora-newspaper-object islandora">
+  <div class="islandora-newspaper-content-wrapper clearfix">
+    <?php if ($islandora_content): ?>
+      <div class="islandora-newspaper-content">
+        <?php print $islandora_content; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+  <div class="islandora-newspaper-metadata">
+    <?php print $description; ?>
+    <?php if ($parent_collections): ?>
+      <div>
+        <h2><?php print t('In collections'); ?></h2>
+        <ul>
+          <?php foreach ($parent_collections as $collection): ?>
+        <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php endif; ?>
+    <?php print $metadata; ?>
+  </div>
+</div>
+
+<script>
+    (function ($) {
+        var percentangeOfWindow = .75;
+         // Use jQuery with the shortcut:
+        // Here we immediately call the function with jQuery as the parameter.
+        $( document ).ready(function() {
+            var $content = $(".vertical-tabs fieldset.vertical-tabs-pane");
+            $.each($content, function(index, data){
+                if($(data).css('display') === 'none'){
+                    $(data).addClass('visually-hidden');
+                }
+            });
+            
+            $('.vertical-tab-button a').click(function(){
+                
+                
+                var tabPosition = $(".vertical-tab-button.selected").offset().top;
+                var containerPosition = $(".vertical-tabs").offset().top;
+                var newMargin =  tabPosition - containerPosition ;
+                $.each($content, function(index, data){
+                    if($(data).css('display') === 'block'){
+                        $(data)[0].style.setProperty( 'margin-top', newMargin + "px", 'important' );
+                        //$("form-wrapper vertical-tabs-pane selected").css('margin-top',tabHeight);
+                        $(data).removeClass('visually-hidden');
+                        
+                    } else {
+                        $(data).addClass('visually-hidden');
+                        $(data).removeClass('selected');
+                    }                 
+                });
+            });
+            
+            var viewportHeight = $( window ).height();
+            
+            // make the tab list % height of the view port
+            $(".vertical-tabs-list").height(Math.round(viewportHeight * percentangeOfWindow));
+        });
+        $( window ).resize(function() {
+            var viewportHeight = $( window ).height();
+            
+            // make the tab list % height of the view port
+            $(".vertical-tabs-list").height(Math.round(viewportHeight * percentangeOfWindow));
+        });
+    }(jQuery));
+   
+
+</script>    
+    
